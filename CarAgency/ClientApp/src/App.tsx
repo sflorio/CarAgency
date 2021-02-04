f3b2ff69db73655f1acb2c35ef4d7a96d876485e
sflorio <sebastianflorio00@hotmail.com>
2021-02-03 23:43:35 -0300

    removed unncesary forms

--- CarAgency/ClientApp/src/App.tsx
+++ CarAgency/ClientApp/src/App.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
 import ListaMarcas from "./components/marca/Marca"
 import ListaVehiculos from "components/vehiculo/Vehiculo";
 import IngresoVehiculo from 'components/vehiculo/IngresoVehiculo';
@@ -12,8 +10,6 @@ import './custom.css'
 export default () => (
     <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
         <Route path='/marca/lista-marcas/:startDateIndex?' component={ListaMarcas} />
         <Route path='/vehiculo/lista-vehiculos/:startDateIndex?' component={ListaVehiculos} />
         <Route path='/vehiculo/ingreso-vehiculo' component={IngresoVehiculo} />
