ccd5dbd6f7fa163e5697388f2d33d402e0a3969a
sflorio <sebastianflorio00@hotmail.com>
2021-02-17 23:42:26 -0300

    added controller TipoVehiculo and linked to autcompleteTipoVehiculo

--- CarAgency/ClientApp/src/store/actions/actionsTipoVehiculo.ts
+++ CarAgency/ClientApp/src/store/actions/actionsTipoVehiculo.ts
@@ -8,22 +8,14 @@ const serviceBaseModel = "TiposVehiculo";
 export const actionCreatorsTipoVehiculo = { 
     getAllTipoVehiculos: () : Promise<TipoVehiculo[]> =>{
 
-        return new Promise((res) =>{
-            return [
-                { TipoVehiculoId: 1 , Descripcion: "Sedan" },
-                { TipoVehiculoId: 2 , Descripcion: "Hatback" },
-                { TipoVehiculoId: 3 , Descripcion: "Monovolumen" },
-                { TipoVehiculoId: 4 , Descripcion: "Camión" }
-            ];
-        })
-        /*return axios
+        return axios
         .get(serviceBaseModel)
         .then( Response => Response )
         .then(Response => Response.data as unknown as TipoVehiculo[])
         .catch(error => {
             console.log(error);
             return [];
-        });*/
+        });
 
     }
 };
--- CarAgency/Controllers/MarcasController.cs
+++ CarAgency/Controllers/MarcasController.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CarAgency.Data;
-using Domain.Models;
 using Domain.Models.Vehiculos;
 
 namespace CarAgency.Controllers
