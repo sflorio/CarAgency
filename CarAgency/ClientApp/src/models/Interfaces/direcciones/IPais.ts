2e03beaa5dbbc07b1721ac0e60cbfe04018fc7dd
sflorio <sebastianflorio00@hotmail.com>
2021-02-22 01:48:55 -0300

    Added folder direcciones

---
+++ CarAgency/ClientApp/src/models/Interfaces/direcciones/IPais.ts
@@ -0,0 +1,4 @@
+export default interface IPais {
+    IdPais: number,
+    Descripcion: string
+}
---
+++ CarAgency/ClientApp/src/models/Interfaces/direcciones/IPartido.ts
@@ -0,0 +1,4 @@
+export default interface IPartido {
+    IdPartido: number,
+    Descripcion: string
+}
