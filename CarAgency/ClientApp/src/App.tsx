dca2b1b60caa5196929e5debad0a9d8b46b0348a
juan palomino <j.palomino92@yahoo.com.ar>
2021-02-23 22:07:09 +0100

    se creo el formulario modelos y se termino la nav

--- CarAgency/ClientApp/src/App.tsx
+++ CarAgency/ClientApp/src/App.tsx
@@ -6,6 +6,7 @@ import ListaMarcas from "./components/marca/Marca"
 import ListaVehiculos from "components/vehiculo/Vehiculo";
 import IngresoVehiculo from 'components/vehiculo/IngresoVehiculo';
 import ListaEstadosCiviles from "components/personas/EstadoCivil";
+import ListaModelo from 'components/modelo/Modelo';
 import './custom.css'
 
 export default () => (
@@ -15,6 +16,8 @@ export default () => (
         <Route path='/vehiculo/lista-vehiculos/:startDateIndex?' component={ListaVehiculos} />
         <Route path='/vehiculo/ingreso-vehiculo' component={IngresoVehiculo} />
         <Route path='/personas/lista-estadoCivil' component={ListaEstadosCiviles} />
+        <Route path='/modelo/lista-modelo' component={ListaModelo} />
+
 
     </Layout>
 );
